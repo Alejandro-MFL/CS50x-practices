d5226cc799ffadaeaf6acf153fb3e97f8f990f24
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-06-18 12:59:49 +0200

    Auto-commit 2025-06-18_12-59-49

--- commit.c
+++ commit.c
@@ -3,14 +3,18 @@
 #include <time.h>
 
 char* getDate(void);
+int autoCommit(char* message);
 
 int main(void)
 {
     //Obtener fecha y hora
     char* fecha = getDate();
-    printf("fecha:  %s", fecha);
+    //Comandos de Git
+    int check = autoCommit(fecha);
     free(fecha);
-    return 0;
+    return check;
+
+
 }
 char* getDate(void) 
 {
@@ -22,4 +26,39 @@ char* getDate(void)
     tm_info = localtime(&t);
     strftime(fecha, 100, "%Y-%m-%d_%H-%M-%S", tm_info);
     return fecha;
+}
+
+int autoCommit(char* message)
+{
+    //Comandos de Git
+    char addChanges[] = "git add .";
+    char commitchanges[200];
+    char pushChanges[] = "git push";
+    snprintf(commitchanges, sizeof(commitchanges), "git commit -m \"Auto-commit %s\"", message);
+    //Los ejecuta
+    int checkAdd = system(addChanges);
+    if (checkAdd != 0)
+    {
+        printf("Error al ejecutar 'git add'\n");
+        return 1;
+    }
+    if (system("git diff --cached --quiet") != 0)
+    {
+        int checkCommit = system(commitchanges);
+        if (checkCommit != 0)
+        {
+            printf("Error al ejecutar 'git commit'\n");
+            return 2;
+        }
+        int checkPush = system(pushChanges);
+        if (checkCommit != 0)
+        {
+            printf("Error al ejecutar 'git push'\n");
+            return 3;
+        }
+        return 0;
+    }
+    printf("no habia cambios\n");
+    return 4;
+    
 }
