c8db72af21b687a54f9cbf540239d6eec9d39a06
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-06-19 20:34:32 +0200

    Commit: 19-06-2025_20:34:32

---
+++ week1/friend.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //Name
+    string name = get_string("what is your name? ");
+
+    // Age
+    int age = get_int("what is your age? ");
+
+    //Hometown
+    string hometown = get_string("what is your hometown? ");
+
+    // Phone number
+    int phoneNumber = get_int("what is your phone number? ");
+
+    printf("Friend's data: \nName: %s\nAge: %i\nHometown: %s\nPhone number: %i \n ", name, age, hometown, phoneNumber);
+
+}
