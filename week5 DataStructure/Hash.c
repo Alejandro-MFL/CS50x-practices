e4eb75937de7feb9232d68149f62d8515da7e3c8
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-06-25 21:16:45 +0200

    Commit: 25-06-2025_21:16:45

--- week5 DataStructure/Hash.c
+++ week5 DataStructure/Hash.c
@@ -5,7 +5,7 @@
 
 typedef struct node
 {
-	char character;
+	char* character;
 	struct node* next;
 }
 node;
@@ -13,18 +13,71 @@ const int ASCIIMAYUS = 32;
 const int ASCIIMIN = 97;
 
 int hash(char* character);
+int addNodes(node** origen, char* newChar);
+void freeMemo(node* list);
+void freeList(node* current);
 
 int main(void)
 {
+	char* word = "hola";
 	node* table[26] = malloc(26,sizeof(node));
+	int index = hash(word);
+	if (index = 28) 
+	{
+		return 1 // no es una palabra
+	}
+
+	
+	addNodes(table[index],word);		
+	
+	
+
+	free(table);
 }
 
+int addNodes(node** origen, char* newChar)
+{
+	node* n = malloc(sizeof(node));
+	if (n == NULL)
+	{
+		return 1;
+	}
+	n->character = newChar;
+	n->next = *origen;
+	*origen = n;
+	return 0;
+}
 
 int hash(char* character)
 {
-	if (character < 97)
+	if (character < 65) 
+	{
+		return 28;
+	}
+	else if (character < 97)
 	{
 		character += ASCIIMAYUS;
 	}
 	return character - ASCIIMIN;
+}
+
+void freeMemo(node* list) 
+{
+	for (int i = 0; i < 26; i++)
+	{
+		if (list[i].character != NULL)
+		{
+			freeList(list[i]);
+		}
+	}
+	free(list);
+}
+
+void freeList(node* current)
+{
+	if (current->next != NULL)
+	{
+		freeList(current->next);
+	}
+	free(current);
 }
