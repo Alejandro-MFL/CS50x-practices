61cea59393ffb5ae449526000ae6ccb61c924a8c
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-06-27 12:41:33 +0200

    Commit: 27-06-2025_12:41:33

--- week6 Python/hello.py
+++ week6 Python/hello.py
@@ -1,43 +1,65 @@
 #Pruebas de elementos similares en python, son pruebas sencillas por lo que no estan comentadas y los valores tienen nombres no descriptivos
-#Hello word con nombre
-answer = input("what's your name? ")
-print(f"hello, {answer}")
+def main():        
+    #Hello word con nombre
+    answer = input("what's your name? ")
+    print(f"hello, {answer}")
 
-#Prueba de comparadores de int con if
-x = int(input("x = "))
-y = int(input("y = "))
+    #Prueba de comparadores de int con if
+    x = int(input("x = "))
+    y = int(input("y = "))
 
-if x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is more than y")
-else:
-    print("x is equal to y")
+    numComparator(x, y)
+    
+    #Comparadores de string con in    
+    agreed()
+        
+    #copia de strings mas mayusculas
+    s = input("s: ")
+    capitalizacion(s)
 
-#Comparadores de string con in
-s = input("Do you agree? ").lower()
+    #bucles de while y for
+    meaw(3)
 
-if s == 'y' or s == 'yes':
-    print("Agreed.")
-elif s in ['no','n']:
-    print("Not agreed.")
-else:
-    print("Please answer only with \"y\" or \"n\"")
-    #Añadir recursion cuando vea creacion de funciones(lo siento si se me olvida)
+def numComparator(x, y):
+    if isinstance(x, int) and isinstance(x, int) or isinstance(x, float) and isinstance(x, float):            
+        if x < y:
+            print("x is less than y")
+        elif x > y:
+            print("x is more than y")
+        else:
+            print("x is equal to y")
+    else:
+        print("Need only numbers, sorry")
 
-#copia de strings
-s = input("s: ")
+def capitalizacion(word): 
+    t = word.capitalize()
 
-t = s.capitalize()
+    print(word)
+    print(t)
 
-print(s)
-print(t)
+def meaw(n):
+    #''' es lo mas parecido a /**/, una lastima, pero la mejor opcion para desactivar funciones temporalmente y similares
+    '''
+    i = 0
+    while i < 3:
+        print("meaw")
+        i +=1
+    '''
+        #el bucle for es definido por rango desde el inicio normalmente con rango pero funciona con listas y similares
+    for j in range(3):
+        print("meaw")
+    
 
-#bucles de while y for
-i = 0
-while i < 3:
-    print("meaw")
-    i +=1
-    #el bucle for es definido por rango desde el inicio normalmente con rango pero funciona con listas y similares
-for j in range(3):
-    print("meaw")
+def agreed():
+        s = input("Do you agree? ").lower()
+
+        if s == 'y' or s == 'yes':
+            print("Agreed.")
+        elif s in ['no','n']:
+            print("Not agreed.")
+        else:
+            print("Please answer only with \"y\" or \"n\"")
+            agreed()
+
+
+main()
