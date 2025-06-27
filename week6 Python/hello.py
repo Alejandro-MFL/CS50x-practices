abf61bb5dc2066e9bcd9b7d52c09e55d33613ebf
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-06-27 10:50:35 +0200

    Commit: 27-06-2025_10:50:35

--- week6 Python/hello.py
+++ week6 Python/hello.py
@@ -1,6 +1,9 @@
+#Pruebas de elementos similares en python
+#Hello word con nombre
 answer = input("what's your name? ")
 print(f"hello, {answer}")
 
+#Prueba de comparadores de int con if
 x = int(input("x = "))
 y = int(input("y = "))
 
@@ -10,3 +13,14 @@ elif x > y:
     print("x is more than y")
 else:
     print("x is equal to y")
+
+#Comparadores de string con in
+
+s = input("Do you agree? ")
+
+if s == 'y' or s == 'Y':
+    print("Agreed.")
+elif s in ['N','n']:
+    print("Not agreed.")
+else:
+    print("Please answer only with \"y\" or \"n\"")
